e22422e32285e2c06aa87492c4a40d743a1e5813
李太新 <toonew@danwi.me>
2016-06-02 18:28:34 +0800

    调整 http 。还是不清楚http 请求的问题

--- http.js
+++ http.js
@@ -3,16 +3,29 @@
  */
 var fs = require('fs');
 var http = require('http');
-
+var querystring = require('querystring')
 var ws = fs.createWriteStream(__dirname + '/test.txt', {start: 0});
 
-for (var i = 0; i < 10; i++) {
-  (function (i) {
-    http.get('www.baidu.com/'+i, (res)=> {
-      // console.log(res);
-      ws.write(res.toString(),'utf-8');
+var data = querystring.stringify({
+  info:'hi',
+  test:5
+});
+
+// for (var i = 0; i < 10; i++) {
+//   (function (i) {
+    var options={
+      host:'www.jb51.net',
+      headers: {
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Content-Length': data.length
+      }
+    };
+
+    http.request(options, (res)=> {
+      console.log(res);
+      // ws.write(res.toString(),'utf-8');
     }).on('error', function (e) {
       console.log("错误" + e.message);
     });
-  })(i)
-}
+  // })(i)
+// }
