506386366bee318a912266228e6becadc3224b7f
李太新 <toonew@danwi.me>
2016-09-12 14:39:13 +0800

    inherits和call

--- util/inherits.js
+++ util/inherits.js
@@ -1,12 +1,43 @@
 /**
  * Created by toonew on 2016/9/7.
  */
-//http://www.runoob.com/nodejs/nodejs-util.html
 const util = require('util');
+
+//inherits 实现的是 对象间原型继承的函数
+function Base() {
+  this.name = 'base';
+  this.base = 1991;
+  this.sayHello = function () {
+    console.log('Hello ' + this.name);
+  }
+}
+Base.prototype.showName = function () {
+  console.log(this.name);
+};
+
+function Sub() {
+  this.name = 'sub';
+}
+
+util.inherits(Sub, Base);
+
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+
+var objSub = new Sub();
+objSub.showName();
+// objSub.sayHello();   //objSub.sayHello is not a function
+console.log(objSub);
+
+/*******   注意：此处继承的只是  原型上的方法  ******************/
+
+//http://www.runoob.com/nodejs/nodejs-util.html
 const EventEmitter = require('events');
 
 function MyStream() {
-  EventEmitter.call(this);
+  EventEmitter.call(this);    //将EventEmitter的构造方法的属性和方法也导入
 }
 
 util.inherits(MyStream, EventEmitter);
@@ -26,28 +57,3 @@ stream.on('data', (data)=> {
 
 stream.write('It works!');
 
-function Base() {
-  this.name = 'base';
-  this.base = 1991;
-  this.sayHello = function () {
-    console.log('Hello ' + this.name);
-  };
-}
-Base.prototype.showName = function () {
-  console.log(this.name);
-};
-
-function Sub() {
-  this.name = 'sub';
-}
-
-util.inherits(Sub, Base);
-var objBase = new Base();
-objBase.showName();
-objBase.sayHello();
-console.log(objBase);
-
-var objSub = new Sub();
-objSub.showName();
-// objSub.sayHello();   //objSub.sayHello is not a function
-console.log(objSub);
