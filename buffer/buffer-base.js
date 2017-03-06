d36993799dab3456469176c42c37a2b934f8348d
李太新 <toonew@danwi.me>
2017-03-06 19:31:28 +0800

    buffer的继承关系 buffer-->Buffer -->Unit8Array -->TypedArray -->Object

---
+++ buffer/buffer-base.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Rain on 2017/2/27.
+ */
+var buffer = new Buffer('s');
+
+console.log(typeof buffer);
+console.log(buffer);
+//一个buffer对象 隐式原型连
+// buffer --> Buffer --> Unit8Array  --> TypedArray --> Object
