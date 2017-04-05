a8cb7fe5f51a1ae9fd59d9f4c4563a83fcb2cf0d
李太新 <toonew@danwi.me>
2017-04-05 14:17:53 +0800

    读stream的一点新认知

--- stream/readStream.js
+++ stream/readStream.js
@@ -10,20 +10,20 @@ var stream = require('stream');
 var rs = stream.Readable();
 rs.push('beep ');
 rs.push('boop \n');
-rs.push(null);
+rs.push(null);   // 已push null 为结尾
 
 //2.为了避免大量数据的缓存，通过定义 _read 实现实时按需数据推送：
-var rs2 = new stream.Readable;
+var rs2 = new stream.Readable();
 var c2 = 97;
 rs2._read = function () {
   rs2.push(String.fromCharCode(c2++));
-  if (c2 > 'z'.charCodeAt(0))rs2.push(null);
+  if (c2 > 'z'.charCodeAt(0)) rs2.push(null);
 };
 
 rs2.pipe(process.stdout);
 
 //3.解释上面只有 在消耗时_read才会呗调用（严重怀疑 这个例子，这个例子真的说明了么？）
-var rs3 = new stream.Readable;
+var rs3 = new stream.Readable();
 var c3 = 97 - 1;
 rs3._read = function () {
   if (c3 >= 'z'.charCodeAt(0))return rs3.push(null);
@@ -42,10 +42,10 @@ process.stdout.on('error', process.exit);
 
 
 //4. 这是通过读取文件 创建可取文件流
-var data = "";
 var readerStream = fs.createReadStream('./example.txt');
 
 readerStream.setEncoding('UTF8');
+var data = "";
 
 // 处理流事件 --> data, end, and error
 readerStream.on('data', function (chunk) {
--- stream/readStream1.js
+++ stream/readStream1.js
@@ -6,7 +6,7 @@ const stream = require('stream');
 var Readable = stream.Readable;
 
 //1.创建 一个 可读流， 用push 写入数据， 在流未被使用之前就已经写入了  内存
-var rs1 = new Readable;
+var rs1 = new Readable();
 
 rs1.push('beep ');
 rs1.push('boop\n');
@@ -14,7 +14,7 @@ rs1.push(null);
 rs1.pipe(process.stdout);
 
 //2. 使用 __read  函数只有 被调用时，才会被调用
-var rs2 = new Readable;
+var rs2 = new Readable();
 
 var c2 = 97;
 rs2._read = function () {
