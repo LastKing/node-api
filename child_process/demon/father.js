15ead930a5b01142806c9e4e8b4545e186a39cc5
李太新 <litaixin@donews.com>
2018-03-19 09:55:06 +0800

    修改增加注释

--- child_process/demon/father.js
+++ child_process/demon/father.js
@@ -2,6 +2,7 @@
  * node创建守护进程 （这个写法与linux完全挂钩，linux和window都不会生效)
  * 源自 饿了么 github知识总结出现的一个题目
  * https://cnodejs.org/topic/57adfadf476898b472247eac
+ * https://nodejs.org/dist/latest-v8.x/docs/api/child_process.html#child_process_options_detached
  * Created by Rain on 2018/3/19
  */
 const spawn = require('child_process').spawn;
